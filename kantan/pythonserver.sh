9cefe301d846e16ee55c1dae832ca0dcab16bfa9
Thomasorus <contact@thomasorus.com>
2020-12-05 01:16:45 +0100

    Added servers

---
+++ kantan/pythonserver.sh
@@ -0,0 +1,6 @@
+# Kantan Python server script
+# This file launches a basic Python server.
+# You can customize it to go to the location of your app and choose which file to launch.
+
+# Example: go to the dist folder
+python3 -m http.server -d demo 8000
